097ba40f3b387c0ad4773a64f69059983e048809
christian-bs <christian.santos3@fatec.sp.gov.br>
2021-02-02 23:12:22 -0300

    Adição do Schema de grade atraves do Mongoose

--- models/index.js
+++ models/index.js
@@ -4,4 +4,30 @@ const db = {};
 db.mongoose = mongoose;
 db.url = process.env.MONGODB;
 
-export { db };
+const gradeSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    value: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    lastModified: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const gradeModel = mongoose.model('grades', gradeSchema, 'grades');
+
+export { db, gradeModel };
